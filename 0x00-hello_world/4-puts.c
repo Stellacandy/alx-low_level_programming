3bed0a2a78133536dd5031930f069157866ead9f
Stella <stella.me90@gmail.com>
2022-06-03 09:25:17 -0700

    cprogram

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+/**                                                                                                                                   
+ *
+ *  * main - Entry point                                                                                                                 
+ *
+ *   * print  Programming is like building a multilingual puzzle                                                                           
+ *
+ *    * Return Always 0 (Success)                                                                                                          
+ *
+ *     */                                                                                                                                   
 
-/**
- * main - Entry point
- * print programming is like building a multilingual puzzle
- * Return Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}
+int main(void)                                                                                                                        
+
+{                                                                                                                                     
+
+	        puts("\"Programming is like building a multilingual puzzle");                                                                 
+
+		        return (0);
