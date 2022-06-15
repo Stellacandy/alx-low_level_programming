738899b5681d0d75d9202e234f0c777b0e697cdc
Stella <stella.me90@gmail.com>
2022-06-15 09:24:24 -0700

    cprogram

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,10 +3,17 @@
 /**
  * _isupper - check for upper case letter
  * @c : character to check the case
- * Return:0 or 1
+ * Return: 0 if sucessful
  */
 
 int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
