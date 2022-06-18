ad08f34c3f715b1985fc825be6f774d2abf74062
Stella <stella.me90@gmail.com>
2022-06-18 09:05:25 -0700

    cprogram

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,29 +3,28 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success/correct)
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
+
 int main(void)
 {
-	int n:
+	int n;
 
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-
-		if (n == 0)
-		{
-			printf("%d is zero\n", n);
-		}
-
-		else if (n < 0)
-		{
-			printf("%d is negative\n", n);
-		}
-
-		else
-		{
-			printf("%d is positive\n", n);
-		}
-		return (0);	}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{ 
+	printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+	printf("%d is zero\n", n);
+}
+if (n < 0)
+{
+	printf("%d is negative\n", n);
+}
+return (0);
+}
