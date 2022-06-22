8dcac0a135b7a6cef4e640f19ffd1750481ccf3f
Stella <stella.me90@gmail.com>
2022-06-22 05:35:35 -0700

    cprogram

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -33,18 +33,16 @@ int p1(char *s, int l)
 	{
 		return (p1(s + 1, l - 2));
 	}
-	return (0);
-}
 
+	/**
+	 * is_palindrome - palindrome
+	 * @s: pointer to string
+	 * Return: recursion
+	 */
 
-/**
- * is_palindrome - palindrome
- * @s: pointer to string
- * Return: recursion
- */
+	int is_palindrome(char *s)
+	{
+		int len = _strlen_recursion(s);
 
-{
-	int len = _strlen_recursion(s);
-
-	return (p1(s, len - 1));
-}
+		return (p1(s, len - 1));
+	}
