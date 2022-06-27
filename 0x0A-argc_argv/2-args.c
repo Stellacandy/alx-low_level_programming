14156462030447020555b8a85c0f45f6d79dc353
Stella <stella.me90@gmail.com>
2022-06-27 03:21:18 -0700

    cprogram

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints allargument passed to the program
+ * @argc: integer that counts number of argument
+ * @argv: an array of char that contains argument passed to the program
+ * Return: Success(0)
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
