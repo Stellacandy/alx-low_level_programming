3d91ff5a5283a8f1e08a96f12377d41306914d9c
Stella <stella.me90@gmail.com>
2022-06-27 03:08:30 -0700

    cprogram

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to a program
+ * @argc: integer that counts number of argument
+ * @argv: an array of char that contains argument passed to the program
+ * Return: Success(0)
+ */
+
+
+int main(int argc, __attribute__((unused))char **argv)
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
