40fbc704e1a4bc8cce4d51b1aaf5af233170adf0
Stella <stella.me90@gmail.com>
2022-09-09 07:19:23 -0700

    task 2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -3,16 +3,13 @@ int add(int a,int b)
 {
 	return a + b;
 }
-
 int sub(int a,int b)
 {
 	return a - b;
 }
-
-
 int mul(int a,int b)
 {
-	 return a * b;
+	return a * b;
 }
 
 int div(int a,int b)
@@ -20,8 +17,7 @@ int div(int a,int b)
 	return a / b;
 }
 
-
 int mod(int a,int b)
 {
-	 return a % b;
+	return a % b;
 }
